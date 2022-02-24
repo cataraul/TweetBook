3b8172aea9744204155df7e249e768230e19bee4
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-02-24 11:00:24 +0200

    Fixed Conflicts

--- TweetBook/Controllers/V1/PostsController.cs
+++ TweetBook/Controllers/V1/PostsController.cs
@@ -7,92 +7,52 @@ using TweetBook.Services;
 
 namespace TweetBook.Controllers
 {
-
-    public class PostsController :ControllerBase
-    {
-        private IList<Post> _posts;
-        public PostsController()
-    public class PostsController :ControllerBase
-    {
-        private IList<Post> _posts;
-        public PostsController()
->>>>>>>>> Temporary merge branch 2
-    public class PostsController :ControllerBase
-    {
-        private IList<Post> _posts;
-        public PostsController()
->>>>>>>>> Temporary merge branch 2
-    public class PostsController :ControllerBase
-    {
-        private IList<Post> _posts;
-        public PostsController()
->>>>>>>>> Temporary merge branch 2
-    public class PostsController : ControllerBase 
+    public class PostsController : ControllerBase
     {
         private readonly IPostService _postService;
         public PostsController(IPostService postService)
->>>>>>>>> Temporary merge branch 2
+
         {
+            _postService = postService;
         }
-        
+
         [HttpGet(ApiRoutes.Posts.GetAll)]
         public IActionResult GetAll()
->>>>>>>>> Temporary merge branch 2
+        {
 
             return Ok(_postService.GetPosts());
         }
 
->>>>>>>>> Temporary merge branch 2
-        public IActionResult Get([FromRoute]Guid postId)
+        [HttpGet(ApiRoutes.Posts.Get)]
+        public IActionResult Get([FromRoute] Guid postId)
         {
 
             var post = _postService.GetPostById(postId);
->>>>>>>>> Temporary merge branch 2
-<<<<<<<<< Temporary merge branch 1
-            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id);
 
-           
-=========
+            if (post == null)
                 return NotFound();
->>>>>>>>> Temporary merge branch 2
 
             return Ok(post);
->>>>>>>>> Temporary merge branch 2
-<<<<<<<<< Temporary merge branch 1
-            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id);
 
-           
-=========
+        }
 
         [HttpPost(ApiRoutes.Posts.Create)]
         public IActionResult Create([FromBody] CreatePostRequest postRequest)
         {
-<<<<<<<<< Temporary merge branch 1
-            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id);
 
-           
-=========
             var post = new Post { Id = postRequest.Id };
 
             if (post.Id == Guid.Empty)
                 post.Id = Guid.NewGuid();
-<<<<<<<<< Temporary merge branch 1
-            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id);
 
-           
-=========
             _postService.GetPosts().Add(post);
 
             var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
 
-<<<<<<<<< Temporary merge branch 1
-            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id);
 
-           
-=========
             var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id.ToString());
 
-            return Created(locationUri,post);
+            return Created(locationUri, post);
         }
     }
 }
