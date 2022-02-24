d91ea13faa531e51a8ae7562e0fd38a18c4eea14
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-02-24 11:20:04 +0200

    Created a Put Request

--- TweetBook/Controllers/V1/PostsController.cs
+++ TweetBook/Controllers/V1/PostsController.cs
@@ -23,6 +23,26 @@ namespace TweetBook.Controllers
             return Ok(_postService.GetPosts());
         }
 
+        [HttpPut(ApiRoutes.Posts.Update)]
+        public IActionResult Update([FromRoute] Guid postId,[FromBody] UpdatePostRequest request)
+        {
+
+            var post = new Post
+            {
+                Id = postId,
+                Name = request.Name,
+            };
+
+           var updated = _postService.UpdatePost(post);
+
+            if(updated)
+                return Ok(post);
+
+            return NotFound();
+
+        }
+
+
         [HttpGet(ApiRoutes.Posts.Get)]
         public IActionResult Get([FromRoute] Guid postId)
         {
