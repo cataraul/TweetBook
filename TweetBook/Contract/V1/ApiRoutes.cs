743a108a28106ef02dcce1899ad84fa35ed8a51e
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-02-24 11:27:08 +0200

    Created a Delete string (API Route)

--- TweetBook/Contract/V1/ApiRoutes.cs
+++ TweetBook/Contract/V1/ApiRoutes.cs
@@ -13,6 +13,8 @@
 
             public const string Update = Base + "/{postId}";
 
+            public const string Delete = Base + "/{postId}";
+
             public const string Get = Base + "posts/{postId}";
 
             public const string Create = Base + "/posts";
