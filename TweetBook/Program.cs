b9bf4c17878d98b40f3a7e495902c94dfb66ecde
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-02-25 13:08:20 +0200

    Set up the JWT Bearer Authentication

--- TweetBook/Program.cs
+++ TweetBook/Program.cs
@@ -1,6 +1,11 @@
 ﻿global using TweetBook.Data;
 global using TweetBook.Services;
 global using Microsoft.EntityFrameworkCore;
+using TweetBook.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +18,61 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<DataContext>(options =>options.UseSqlite(connectionString));
 
 
+//Bearer Token Configuration
+var jwtSettings = new JwtSettings();
+builder.Configuration.Bind(key:nameof(jwtSettings),jwtSettings);
+builder.Services.AddSingleton(jwtSettings);
+
+builder.Services.AddAuthentication(configureOptions: x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(x =>
+    {
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key:Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            RequireExpirationTime = false,
+            ValidateLifetime = true
+        };
+    });
+
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+{
+    var security = new Dictionary<string, IEnumerable<string>>
+    {
+        {"Bearer",new string[0]}
+    };
+    x.AddSecurityDefinition(name: "Bearer", new OpenApiSecurityScheme
+    {
+        Description= "JWT Authorization header using the bearer scheme",
+        Name = "Authorization",
+        In=ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey
+    });
+    x.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+     {
+        new OpenApiSecurityScheme
+          {
+               Reference = new OpenApiReference
+                 {
+                   Type = ReferenceType.SecurityScheme,
+                   Id = "Bearer"
+                  }
+          },
+       Array.Empty<string>()
+       }
+    });
+});
 
 var app = builder.Build();
 
@@ -29,6 +86,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
