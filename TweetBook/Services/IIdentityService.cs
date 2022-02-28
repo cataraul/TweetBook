158b66dd3b92c25afee3c8d1eb1ce0997e6ae8f7
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-02-28 11:30:38 +0200

    Implemented Login using JWT

--- TweetBook/Services/IIdentityService.cs
+++ TweetBook/Services/IIdentityService.cs
@@ -5,5 +5,6 @@ namespace TweetBook.Services
     public interface IIdentityService
     {
       public  Task<AuthenticationResult> RegisterAsync(string email, string password);
+      public  Task<AuthenticationResult> LoginAsync(string email, string password);
     }
 }
--- TweetBook/Services/IdentityService.cs
+++ TweetBook/Services/IdentityService.cs
@@ -36,7 +36,7 @@ namespace TweetBook.Services
                 UserName = email,
             };
 
-            var createdUser = await _userManager.CreateAsync(newUser,password);
+            var createdUser = await _userManager.CreateAsync(newUser, password);
 
             if (!createdUser.Succeeded)
             {
@@ -45,7 +45,35 @@ namespace TweetBook.Services
                     Errors = createdUser.Errors.Select(x => x.Description)
                 };
             }
+            return GenerateAuthenticationResultForUser(newUser);
+        }
 
+        public async Task<AuthenticationResult> LoginAsync(string email,string password)
+        {
+            var user  = await _userManager.FindByEmailAsync(email);
+
+            if(user==null)
+            {
+                return new AuthenticationResult
+                {
+                    Errors = new[] { "User does not exist" }
+                };
+            }
+            var userHasValidPassword = await _userManager.CheckPasswordAsync(user, password);
+
+            if (!userHasValidPassword)
+            {
+                return new AuthenticationResult
+                {
+                    Errors = new[] { "User/password combination is wrong" }
+                };
+            }
+
+            return GenerateAuthenticationResultForUser(user);
+        }
+
+        private AuthenticationResult GenerateAuthenticationResultForUser(IdentityUser newUser)
+        {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
             var tokenDesriptor = new SecurityTokenDescriptor
@@ -58,7 +86,7 @@ namespace TweetBook.Services
                     new Claim(type:"id",newUser.Id)
                 }),
                 Expires = DateTime.UtcNow.AddHours(2),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha256Signature),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
             };
 
             var token = tokenHandler.CreateToken(tokenDesriptor);
