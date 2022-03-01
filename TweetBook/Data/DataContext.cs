a7df269d3bcedcbaa8fe9f9b3654a241608a0e61
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-03-01 15:51:07 +0200

    Created DbSet for refreshTokens

--- TweetBook/Data/DataContext.cs
+++ TweetBook/Data/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using TweetBook.Domain;
 
 namespace TweetBook.Data
@@ -9,5 +10,6 @@ namespace TweetBook.Data
         {
         }
         public DbSet<Post> Posts { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
     }
 }
