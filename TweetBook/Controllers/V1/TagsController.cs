0e44003e6085dd8280ade276e4679f6792b2da60
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-03-02 15:55:45 +0200

    Create TagsController.cs

---
+++ TweetBook/Controllers/V1/TagsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TweetBook.Contract;
+
+namespace TweetBook.Controllers.V1
+{
+    [Authorize(AuthenticationSchemes=JwtBearerDefaults.AuthenticationScheme)]
+    public class TagsController : ControllerBase
+    {
+        private readonly IPostService _postService;
+
+        public TagsController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet(ApiRoutes.Tags.GetAll)]
+
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _postService.GetAllAsync());
+        }
+    }
+}
