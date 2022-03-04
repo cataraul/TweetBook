3389ca38a056e2c6cffe3d1ce8b7c9b7a0c2f459
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-03-04 17:05:04 +0200

    ApiKey-based Authentication

---
+++ TweetBook/Data/Migrations/20220304140535_posttagTag.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TweetBook.Data.Migrations
+{
+    public partial class posttagTag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PostTags_Tags_TagName",
+                table: "PostTags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PostTags_TagName",
+                table: "PostTags");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PostTags_TagName",
+                table: "PostTags",
+                column: "TagName");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PostTags_Tags_TagName",
+                table: "PostTags",
+                column: "TagName",
+                principalTable: "Tags",
+                principalColumn: "Name",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
