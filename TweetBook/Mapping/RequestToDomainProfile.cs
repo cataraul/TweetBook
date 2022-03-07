db757b969a4bd4469ab5161b9b10294858e72f15
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-03-07 17:35:51 +0200

    Implemented profile

---
+++ TweetBook/Mapping/RequestToDomainProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Tweetbook.Contracts.V1.Requests.Queries;
+using TweetBook.Domain;
+
+namespace TweetBook.Mapping
+{
+    public class RequestToDomainProfile : Profile
+    {
+        public RequestToDomainProfile()
+        {
+            CreateMap<PaginationQuery, PaginationFilter>();
+        }
+
+    }
+}
