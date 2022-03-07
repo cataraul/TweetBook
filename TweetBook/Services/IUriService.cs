65da885311d36866d9daf74c190384092d477fb1
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-03-07 17:34:10 +0200

    Created Uri Services

---
+++ TweetBook/Services/IUriService.cs
@@ -0,0 +1,11 @@
+﻿using Tweetbook.Contracts.V1.Requests.Queries;
+
+namespace TweetBook.Services
+{
+    public interface IUriService
+    {
+        Uri GetPostUri(string postId);
+
+        Uri GetAllPostsUri(PaginationQuery paginationQuery = null);
+    }
+}
