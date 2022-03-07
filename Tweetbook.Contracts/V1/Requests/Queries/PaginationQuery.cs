55552f75e05ac624ab60e0c37af95d97a65e8557
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-03-07 17:34:59 +0200

    Create PaginationQuery.cs

---
+++ Tweetbook.Contracts/V1/Requests/Queries/PaginationQuery.cs
@@ -0,0 +1,21 @@
+﻿namespace Tweetbook.Contracts.V1.Requests.Queries
+{
+    public class PaginationQuery
+    {
+        public PaginationQuery()
+        {
+            PageNumer = 1;
+            PageSize = 100;
+        }
+
+        public PaginationQuery(int pageNumber, int pageSize)
+        {
+            PageNumer = pageNumber;
+            PageSize = pageSize;
+        }
+
+        public int PageNumer { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
