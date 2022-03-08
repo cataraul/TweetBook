f4fcdf8f4f8709380c142ab967fe086bcfce8499
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-03-08 09:03:55 +0200

    Create RedisHealthChecks.cs

---
+++ TweetBook/HealthChecks/RedisHealthChecks.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using StackExchange.Redis;
+
+namespace TweetBook.HealthChecks
+{
+    public class RedisHealthChecks : IHealthCheck
+    {
+        private readonly IConnectionMultiplexer _connectionMultiplexer;
+
+        public RedisHealthChecks(IConnectionMultiplexer connectionMultiplexer)
+        {
+            _connectionMultiplexer = connectionMultiplexer;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var database = _connectionMultiplexer.GetDatabase();
+                database.StringGet("health");
+                return Task.FromResult(HealthCheckResult.Healthy());
+            }
+            catch(Exception exception)
+            {
+                return Task.FromResult(HealthCheckResult.Unhealthy(exception.Message));
+            }
+        }
+    }
+}
