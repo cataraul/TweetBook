d8196723102e741ba9a160e6ec110d47a1ec0bea
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-03-08 09:04:20 +0200

    HealthChecksInstaller

---
+++ TweetBook/Installers/HealthChecksInstaller.cs
@@ -0,0 +1,14 @@
+﻿using TweetBook.HealthChecks;
+
+namespace TweetBook.Installers
+{
+    public class HealthChecksInstaller : IInstaller
+    {
+        void IInstaller.InstallServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddHealthChecks()
+                .AddDbContextCheck<DataContext>()
+                .AddCheck<RedisHealthChecks>("Redis");
+        }
+    }
+}
