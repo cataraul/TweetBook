fd1f5a24b612e0ffd7c1dc78d7309dc85b49e676
Raul-Catalin Domnita <raul.domnita@yahoo.com>
2022-03-14 16:58:28 +0200

    Not Working Test

---
+++ Tweetbok.IntegrationTest/IntegrationTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using TweetBook.Contract;
+using TweetBook.Contract.V1.Requests;
+using TweetBook.Contract.V1.Responses;
+using TweetBook.Data;
+
+namespace Tweetbok.IntegrationTests
+{
+    public class IntegrationTests : IDisposable
+    {
+        protected readonly HttpClient TestClient;
+        private readonly IServiceProvider serviceProvider;
+
+        protected IntegrationTests()
+        {
+            var appFactory = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(DbContextOptions<DataContext>));
+                        services.AddDbContext<DataContext>(options =>
+                        {
+                            options.UseInMemoryDatabase(databaseName: "TestDb");
+                        });
+                    });
+                });
+            serviceProvider = appFactory.Services;
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task<AuthenticationHeaderValue> AuthenticateAsync()
+        {
+           return TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+
+        protected async Task<PostResponse> CreatePostAsync(CreatePostRequest request)
+        {
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Posts.Create, request);
+
+            return await response.Content.ReadAsAsync<PostResponse>();
+        }
+
+        private async Task<string> GetJwtAsync()
+        {
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Identity.Register, new UserRegistrationRequest
+            {
+                Email = "test@rauls.com",
+                Password = "SomePass1234!"
+            });
+
+            //var registrationResponse = await response.Content.ReadAsAsync<AuthSuccesResponse>();
+            //return registrationResponse.Token;
+            var registrationResponse = await response.Content.ReadAsStringAsync();
+            AuthSuccesResponse resp = JsonConvert.DeserializeObject<AuthSuccesResponse>(registrationResponse);
+            return resp.Token;
+        }
+
+        public void Dispose()
+        {
+            using var serviceScope = serviceProvider.CreateScope();
+            var context = serviceScope.ServiceProvider.GetRequiredService<DataContext>();
+            context.Database.EnsureDeleted();
+        }
+    }
+}
--- Tweetbok.IntegrationTest/PostsControllerTests.cs
+++ Tweetbok.IntegrationTest/PostsControllerTests.cs
@@ -4,12 +4,13 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using TweetBook.Contract;
+using TweetBook.Contract.V1.Requests;
 using TweetBook.Domain;
 using Xunit;
 
-namespace Tweetbok.IntegrationTest
+namespace Tweetbok.IntegrationTests
 {
-    public class PostsControllerTests : IntegrationTest
+    public class PostsControllerTests : IntegrationTests
     {
         [Fact]
         public async Task GetAll_WithoutAnyPosts_ReturnsEmptyResponse()
@@ -23,6 +24,25 @@ namespace Tweetbok.IntegrationTest
             //Assert
             response.StatusCode.Should().Be(HttpStatusCode.OK);
             (await response.Content.ReadAsAsync<List<Post>>()).Should().BeEmpty();
+            //Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            //Assert.Empty(await response.Content.ReadAsAsync<List<Post>>());
+        }
+
+        [Fact]
+        public async Task Get_ReturnsPostWhenPostExistInTheDatabase()
+        {
+            //Arrange
+            var auth = await AuthenticateAsync();
+            var createdPost = await CreatePostAsync(new CreatePostRequest { Name = "test post", Tags = new string[] { "test tag" }});
+
+            //Act
+            var responseGetAll = await TestClient.GetAsync(ApiRoutes.Posts.GetAll);
+            var response = await TestClient.GetAsync(ApiRoutes.Posts.Get.Replace("{postId}", createdPost.Id.ToString()));
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var returnedPost = await response.Content.ReadAsAsync<Post>();
+            returnedPost.Id.Should().Be(createdPost.Id);
+            returnedPost.Name.Should().Be("Test Post");
         }
     }
 }
